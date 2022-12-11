3470da7fe344c47f39a7126eb70652bd6e912a96
matilda-minasian <matildam.1914@gmail.com>
2022-12-10 16:50:04 -0800

    Change code

--- src/App.js
+++ src/App.js
@@ -98,28 +98,34 @@ const App = ({ signOut }) => {
       <Heading level={2}>Current Notes</Heading>
       <View margin="3rem 0">
       {notes.map((note) => (
-  <Flex
-    key={note.id || note.name}
-    direction="row"
-    justifyContent="center"
-    alignItems="center"
-  >
-    <Text as="strong" fontWeight={700}>
-      {note.name}
-    </Text>
-    <Text as="span">{note.description}</Text>
-    {note.image && (
-      <Image
-        src={note.image}
-        alt={`visual aid for ${notes.name}`}
-        style={{ width: 400 }}
-      />
-    )}
-    <Button variation="link" onClick={() => deleteNote(note)}>
-      Delete note
-    </Button>
-  </Flex>
+        <Flex
+          key={note.id || note.name}
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+        <Text as="strong" fontWeight={700}>
+          {note.name}
+        </Text>
+        <Text as="span">{note.description}</Text>
+        {note.image && (
+          <Image
+            src={note.image}
+            alt={`visual aid for ${notes.name}`}
+            style={{ width: 400 }}
+          />
+        )}
+          <Button variation="link" onClick={() => deleteNote(note)}>
+            Delete note
+          </Button>
+        </Flex>
 ))}
+      <View
+        name="image"
+        as="input"
+        type="file"
+        style={{ alignSelf: "end" }}
+/>
       </View>
       <Button onClick={signOut}>Sign Out</Button>
     </View>
